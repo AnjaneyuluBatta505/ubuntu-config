d0105436a50a0cb943e50d3146ef9db2f9d18f44
Anjaneyulu Batta <anjaneyulu.batta505@gmail.com>
2019-11-17 19:49:14 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+cd ~/ && git clone git@github.com:AnjaneyuluBatta505/ubuntu-config.git && cd ubuntu-config && cp .config ~/.config & cp .tmux.conf ~/.tmux.conf && rm -r ~/ubuntu-config
+openbox-lxde --reconfigure
